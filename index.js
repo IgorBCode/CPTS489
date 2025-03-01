f03462a5f7589b327db8d9ca6556688c58f4ffc4
Igor <igorbara@msn.com>
2025-02-28 21:25:44 -0800

    updated page layouts and styling

--- index.js
+++ index.js
@@ -6,3 +6,12 @@ function toggleReplyBox(commentId) {
         replyBox.style.display = "none";
     }
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+    fetch('sidebar.html')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('sidebar').innerHTML = data;
+    })
+    .catch(error => console.error('Error loading sidebar:', error));
+});
